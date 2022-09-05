6fb3a0671d4c4e84bfe5d89ff3ffc5ff924bff20
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-09-05 04:17:52 +0200

    add url prefix replacement utilities functions to base model

--- lib/ontologies_linked_data/models/base.rb
+++ lib/ontologies_linked_data/models/base.rb
@@ -137,7 +137,27 @@ module LinkedData
         included_aggregates
       end
 
+      def self.replace_url_prefix_to_id(id)
+        if replace_url_prefix?(id)
+          id = RDF::IRI.new(id.to_s.sub(LinkedData.settings.rest_url_prefix, LinkedData.settings.id_url_prefix))
+        end
+        id
+      end
+
+      def self.replace_url_id_to_prefix(id)
+        if replace_url_prefix?(id)
+          id.to_s.gsub(LinkedData.settings.id_url_prefix, LinkedData.settings.rest_url_prefix)
+        else
+          id.to_s
+        end
+      end
+
+      def self.replace_url_prefix?(id)
+        LinkedData.settings.replace_url_prefix && id.to_s.start_with?(LinkedData.settings.rest_url_prefix)
+      end
       private
+
+
 
       ##
       # Looks for an object 'owner' and looks in Thread.current[:remote_user]
