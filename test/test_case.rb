637590ff5bc9b4bafa70a0d0ad21cc72c0e07e29
Syphax bouazzouni <gs_bouazzouni@esi.dz>
2024-01-29 23:38:15 +0100

    Feature: Add Virtuso, Allegrograph and Graphdb integration  to OLD  (#106)
    * setup multi-store unit-tests environment
    
    * fix unit tests
    
    * add vo parsing optimization

--- test/test_case.rb
+++ test/test_case.rb
@@ -240,7 +240,11 @@ module LinkedData
       raise StandardError, 'Too many triples in KB, does not seem right to run tests' unless
             count_pattern('?s ?p ?o') < 400000
 
-      Goo.sparql_update_client.update('DELETE {?s ?p ?o } WHERE { ?s ?p ?o }')
+      graphs = Goo.sparql_query_client.query("SELECT DISTINCT  ?g WHERE  { GRAPH ?g { ?s ?p ?o . } }")
+      graphs.each_solution do |sol|
+        Goo.sparql_data_client.delete_graph(sol[:g])
+      end
+
       LinkedData::Models::SubmissionStatus.init_enum
       LinkedData::Models::OntologyType.init_enum
       LinkedData::Models::OntologyFormat.init_enum
