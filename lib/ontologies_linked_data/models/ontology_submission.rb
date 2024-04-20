ddeee2c650a0857466503f0f5052ef619d270c04
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2024-04-20 21:15:32 +0200

    extract the submission properties indexation step into a file

--- lib/ontologies_linked_data/models/ontology_submission.rb
+++ lib/ontologies_linked_data/models/ontology_submission.rb
@@ -1051,48 +1051,6 @@ module LinkedData
         end
       end
 
-      def index_properties(logger, commit = true, optimize = true)
-        page = 1
-        size = 2500
-        count_props = 0
-
-        time = Benchmark.realtime do
-          self.bring(:ontology) if self.bring?(:ontology)
-          self.ontology.bring(:acronym) if self.ontology.bring?(:acronym)
-          logger.info("Indexing ontology properties: #{self.ontology.acronym}...")
-          t0 = Time.now
-          self.ontology.unindex_properties(commit)
-          logger.info("Removed ontology properties index in #{Time.now - t0} seconds."); logger.flush
-
-          props = self.ontology.properties
-          count_props = props.length
-          total_pages = (count_props / size.to_f).ceil
-          logger.info("Indexing a total of #{total_pages} pages of #{size} properties each.")
-
-          props.each_slice(size) do |prop_batch|
-            t = Time.now
-            LinkedData::Models::OntologyProperty.indexBatch(prop_batch)
-            logger.info("Page #{page} of ontology properties indexed in #{Time.now - t} seconds."); logger.flush
-            page += 1
-          end
-
-          if commit
-            t0 = Time.now
-            LinkedData::Models::OntologyProperty.indexCommit(nil)
-            logger.info("Ontology properties index commit in #{Time.now - t0} seconds.")
-          end
-        end
-        logger.info("Completed indexing ontology properties of #{self.ontology.acronym} in #{time} sec. Total of #{count_props} properties indexed.")
-        logger.flush
-
-        if optimize
-          logger.info("Optimizing ontology properties index...")
-          time = Benchmark.realtime do
-            LinkedData::Models::OntologyProperty.indexOptimize(nil)
-          end
-          logger.info("Completed optimizing ontology properties index in #{time} seconds.")
-        end
-      end
 
       # Override delete to add removal from the search index
       # TODO: revise this with a better process
