d893c08a2c2f11c743fad520873b7f98b0805240
Syphax <gs_bouazzouni@esi.dz>
2024-08-30 10:15:27 +0200

    add the serialize_default for the portal config model

--- lib/ontologies_linked_data/models/portal_config.rb
+++ lib/ontologies_linked_data/models/portal_config.rb
@@ -8,8 +8,15 @@ module LinkedData
       attribute :description, namespace: :dcterms
       attribute :logo, namespace: :foaf, enforce: [:url]
       attribute :numberOfArtefacts, namespace: :mod, handler: :ontologies_count
-      attribute :federated_portals
-      attribute :fundedBy, enforce: [:list]
+      attribute :federated_portals, handler: :federated_portals_settings
+      attribute :fundedBy, namespace: :foaf, enforce: [:list]
+
+      serialize_default :acronym, :title, :color, :description, :logo, :numberOfArtefacts, :federated_portals, :fundedBy
+
+      def initialize(*args)
+        super
+        init_federated_portals_settings
+      end
 
       def self.current_portal_config
         p = LinkedData::Models::PortalConfig.new
@@ -20,29 +27,31 @@ module LinkedData
         p.color = LinkedData.settings.color
         p.logo = LinkedData.settings.logo
         p.fundedBy = LinkedData.settings.fundedBy
-        p.federated_portals = LinkedData.settings.federated_portals
         p
       end
 
+      def init_federated_portals_settings(federated_portals = nil)
+        @federated_portals = federated_portals || LinkedData.settings.federated_portals.symbolize_keys
+      end
+
+      def federated_portals_settings
+        @federated_portals
+      end
+
       def ontologies_count
-        if current_portal?
-          LinkedData::Models::Ontology.where(viewingRestriction: 'public').count
-        else
-          0
-        end
+        LinkedData::Models::Ontology.where(viewingRestriction: 'public').count
       end
 
       def self.valid_hash_code(inst, attr)
         inst.bring(attr) if inst.bring?(attr)
         str = inst.send(attr)
 
-        unless /^#(?:[0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/ === str do
-          return [:valid_hash_code, "Invalid hex color code: '#{str}'. Please provide a valid hex code in the format '#FFF' or '#FFFFFF'."]
-        end
-        end
-
+        return if (/^#(?:[0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/ === str)
+        [:valid_hash_code,
+         "Invalid hex color code: '#{str}'. Please provide a valid hex code in the format '#FFF' or '#FFFFFF'."]
       end
     end
   end
 end
+
 
