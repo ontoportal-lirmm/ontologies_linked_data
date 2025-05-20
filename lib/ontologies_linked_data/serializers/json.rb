faaf44416bba3f8cdccb423c6759730a22c9bf1e
Imad Bourouche <ji_bourouche@esi.dz>
2025-05-20 12:06:45 +0200

    change links from global to local && change catalog links to use /mod-api namespace

--- lib/ontologies_linked_data/serializers/json.rb
+++ lib/ontologies_linked_data/serializers/json.rb
@@ -18,10 +18,10 @@ module LinkedData
       
       def self.serialize_mod_objects(obj, options = {})
         # using one context and links in mod objects
-        global_links, global_context = {}, {}
+        global_context = {}
 
         hash = obj.to_flex_hash(options) do |hash, hashed_obj|
-          process_common_serialization(hash, hashed_obj, options, global_links, global_context)
+          process_common_serialization(hash, hashed_obj, options, global_context)
         end
 
         result = {}
@@ -32,19 +32,18 @@ module LinkedData
         end
         result.merge!(global_context) unless global_context.empty?
         result.merge!(hash) if hash.is_a?(Hash)
-        result.merge!(global_links) unless global_links.empty?
         MultiJson.dump(result)
       end
 
       
       private
       
-      def self.process_common_serialization(hash, hashed_obj, options, global_links= nil, global_context= nil)
+      def self.process_common_serialization(hash, hashed_obj, options, global_context= nil)
         current_cls = hashed_obj.respond_to?(:klass) ? hashed_obj.klass : hashed_obj.class
         result_lang = get_languages(get_object_submission(hashed_obj), options[:lang])
 
         add_id_and_type(hash, hashed_obj, current_cls)
-        add_links(hash, hashed_obj, options, global_links) if generate_links?(options)
+        add_links(hash, hashed_obj, options) if generate_links?(options)
         add_context(hash, hashed_obj, options, current_cls, result_lang, global_context) if generate_context?(options)
       end
 
@@ -55,19 +54,11 @@ module LinkedData
         hash["@type"] = type(current_cls, hashed_obj) if hash["@id"]
       end
 
-      def self.add_links(hash, hashed_obj, options, global_links)
-        return if global_links&.any?
-        
+      def self.add_links(hash, hashed_obj, options)
         links = LinkedData::Hypermedia.generate_links(hashed_obj)
         return if links.empty?
-
-        if global_links.nil?
-          hash["links"] = links
-          hash["links"].merge!(generate_links_context(hashed_obj)) if generate_context?(options)
-        elsif global_links.empty?
-          global_links["links"] = links
-          global_links["links"].merge!(generate_links_context(hashed_obj)) if generate_context?(options)
-        end
+        hash["links"] = links
+        hash["links"].merge!(generate_links_context(hashed_obj)) if generate_context?(options)
       end
 
       def self.add_context(hash, hashed_obj, options, current_cls, result_lang, global_context)
