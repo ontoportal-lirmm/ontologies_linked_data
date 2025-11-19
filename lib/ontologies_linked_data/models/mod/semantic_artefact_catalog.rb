80cc15801f8ac8f6689be6c1df76d95faa140c84
imadbourouche <ji_bourouche@esi.dz>
2025-11-19 11:18:27 +0100

    Add sampleQueries to ontology and catalog models

--- lib/ontologies_linked_data/models/mod/semantic_artefact_catalog.rb
+++ lib/ontologies_linked_data/models/mod/semantic_artefact_catalog.rb
@@ -40,6 +40,7 @@ module LinkedData
             attribute :bugDatabase, namespace: :doap, enforce: [:string]
             attribute :relation, namespace: :dcterms, enforce: [:string]
             attribute :hasPolicy, namespace: :mod, enforce: [:string]
+            attribute :sampleQueries, namespace: :mod, enforce: [:list, :string]
 
             attribute :created, namespace: :dcterms, enforce: [:date]
             attribute :curatedOn, namespace: :pav, enforce: [:date]
@@ -173,7 +174,7 @@ module LinkedData
             serialize_default :acronym, :title, :color, :description, :logo,:identifier, :status, :language, :type, :accessRights, :license, :rightsHolder,
                               :landingPage, :keyword, :bibliographicCitation, :created, :modified , :contactPoint, :creator, :contributor,
                               :publisher, :subject, :coverage, :createdWith, :accrualMethod, :accrualPeriodicity, :wasGeneratedBy, :accessURL,
-                              :numberOfArtefacts, :federated_portals, :fundedBy
+                              :numberOfArtefacts, :federated_portals, :fundedBy, :sampleQueries
 
             embed :rightsHolder, :contactPoint, :creator, :contributor, :curatedBy, :translator, :publisher, :endorsedBy
 
