8230ef93a4339a2c269f3893b00c43cc727c7510
imadbourouche <ji_bourouche@esi.dz>
2026-02-12 16:50:45 +0100

    fix: add category acronym validation

--- lib/ontologies_linked_data/models/category.rb
+++ lib/ontologies_linked_data/models/category.rb
@@ -2,7 +2,7 @@ module LinkedData
   module Models
     class Category < LinkedData::Models::Base
       model :category, name_with: :acronym
-      attribute :acronym, enforce: [:unique, :existence]
+      attribute :acronym, enforce: [:unique, :existence, :validate_acronym]
       attribute :name, enforce: [:existence]
       attribute :description
       attribute :created, enforce: [:date_time], default: lambda { |record| DateTime.now }
@@ -11,6 +11,13 @@ module LinkedData
 
       serialize_default :acronym, :name, :description, :created, :parentCategory, :ontologies
       cache_timeout 86400
+
+      def validate_acronym(inst, attr)
+        inst.bring(attr) if inst.bring?(attr)
+        acronym = inst.send(attr)
+        return acronym.match?(/^[A-Z][A-Z0-9_-]*$/) ? [] : [:validate_acronym, "`acronym` must be uppercase letters, numbers, underscores or hyphens only and must start with a letter"]
+      end
+
     end
   end
 end
