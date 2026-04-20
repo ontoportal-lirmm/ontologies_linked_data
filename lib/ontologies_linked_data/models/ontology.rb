6105deb3ea7176aa84e16f65ef81779d1adc7e14
Kouicem Wail <kw_kouicem@esi.dz>
2026-04-20 14:23:40 +0200

    feat: add links.ui to Project and Agent models (related issue #176) (#247)
    * feat: add links.ui to Project and Agent models (#176)
    
    Add hypermedia 'ui' link to Project and Agent API responses,
    pointing to the corresponding portal page.
    
    - Project: http://<ui_host>/projects/<acronym>
    - Agent:   http://<ui_host>/agents/<uuid>
    
    Follows the same pattern already used by the Ontology model.
    Resolves #176
    
    * fix remove links_load/add https

--- lib/ontologies_linked_data/models/ontology.rb
+++ lib/ontologies_linked_data/models/ontology.rb
@@ -80,7 +80,7 @@ module LinkedData
               LinkedData::Hypermedia::Link.new("analytics", lambda {|s| "ontologies/#{s.acronym}/analytics"}, "#{Goo.namespaces[:metadata].to_s}Analytics"),
               LinkedData::Hypermedia::Link.new("agents", lambda {|s| "ontologies/#{s.acronym}/agents"}, LinkedData::Models::Agent.uri_type),
               LinkedData::Hypermedia::Link.new("mappings", lambda {|s| "ontologies/#{s.acronym}/mappings"}, LinkedData::Models::Mapping.type_uri),
-              LinkedData::Hypermedia::Link.new("ui", lambda {|s| "http://#{LinkedData.settings.ui_host}/ontologies/#{s.acronym}"}, self.uri_type)
+              LinkedData::Hypermedia::Link.new("ui", lambda {|s| "https://#{LinkedData.settings.ui_host}/ontologies/#{s.acronym}"}, self.uri_type)
 
       # Access control
       read_restriction lambda {|o| !o.viewingRestriction.eql?("public") }
