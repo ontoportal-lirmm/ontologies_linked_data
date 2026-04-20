09ae786a5705a1cc5ca75ede2fcd206fff7cafa5
Kouicem Wail <kw_kouicem@esi.dz>
2026-04-20 14:23:40 +0200

    feat: add links.ui to Project and Agent models (related issue #176) (#247)
    * feat: add links.ui to Project and Agent models (#176)
    
    Add hypermedia 'ui' link to Project and Agent API responses,
    pointing to the corresponding portal page.
    
    - Project: http://<ui_host>/projects/<acronym>
    - Agent:   http://<ui_host>/agents/<uuid>
    
    Follows the same pattern already used by the Ontology model.
    Resolves #176
    
    * fix remove links_load/add https

--- lib/ontologies_linked_data/models/project.rb
+++ lib/ontologies_linked_data/models/project.rb
@@ -12,7 +12,7 @@ module LinkedData
       attribute :contacts
       attribute :institution
       attribute :ontologyUsed, enforce: [:ontology, :list]
+      link_to LinkedData::Hypermedia::Link.new("ui", lambda { |p| "https://#{LinkedData.settings.ui_host}/projects/#{p.acronym}" }, self.uri_type)
     end
   end
 end
-
